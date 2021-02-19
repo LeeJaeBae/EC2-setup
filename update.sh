69d0ee797485c4dcef0ffafe54e9ec60d7a041ce
LeeJaeBae <nea4182@gmail.com>
2021-02-19 16:23:11 +0900

    zsh setup

--- update.sh
+++ update.sh
@@ -6,3 +6,12 @@ sudo apt install curl
 
 sudo apt install software-properteis-common
 
+sudo apt install zsh -y
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo "ZSH_THEME='agnoster'" >> ~/.zshrc
+chsh -s /usr/bin/zsh
+source ~/.zshrc
+
+
